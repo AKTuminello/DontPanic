df67c068135b8cb2175c80df8b358d97e9733d65
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-07-29 18:10:57 -0600

    Working FunStuffScreen emulator and phone

--- firebaseConfig.jsx
+++ firebaseConfig.jsx
@@ -1,12 +1,23 @@
-
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.REACT_NATIVE_API_KEY,
+  authDomain: process.env.REACT_NATIVE_AUTH_DOMAIN,
+  projectId: process.env.REACT_NATIVE_PROJECT_ID,
+  storageBucket: process.env.REACT_NATIVE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_NATIVE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_NATIVE_APP_ID,
 };
 
-export default firebaseConfig;
+let app;
+
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
+}
+
+const db = getFirestore(app);
+
+export { db };
--- src/screens/FunStuffScreen.jsx
+++ src/screens/FunStuffScreen.jsx
@@ -1,31 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { Audio } from 'expo-av';
-import { firebase } from '../../firebaseConfig';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
 
 const FunStuffScreen = () => {
   const [selectedBlend, setSelectedBlend] = useState(null);
+  const [defaultBlend, setDefaultBlend] = useState(null);
   const [audio, setAudio] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [blendsMenu, setBlendsMenu] = useState([]);
 
-  const blendsMenu = [
-    {
-      label: 'Calm',
-      blend: 'calmBlend',
-      text: 'Relax and wind down with this soothing blend.',
-      imageUrl: 'url_to_calm_blend_image',
-      audioUrl: 'url_to_calm_blend_audio',
-    },
-    {
-      label: 'Energized',
-      blend: 'energizedBlend',
-      text: 'Increase alertness and improve focus with this refreshing blend.',
-      imageUrl: 'url_to_energized_blend_image',
-      audioUrl: 'url_to_energized_blend_audio',
-    },
-    // Add more blend objects for other options here
-  ];
+  useEffect(() => {
+    fetchDefaultBlend();
+    fetchBlendsFromFirestore();
+  }, []);
+
+  const fetchDefaultBlend = async () => {
+    try {
+      const defaultBlendId = await AsyncStorage.getItem('defaultBlend');
+      if (defaultBlendId) {
+        const docRef = doc(db, 'OilBlends', defaultBlendId);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setDefaultBlend(docSnap.data());
+        } else {
+          console.log('No such document!');
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching default blend:', error);
+    }
+  };
+
+  const fetchBlendsFromFirestore = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'OilBlends'));
+      const blends = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setBlendsMenu(blends);
+    } catch (error) {
+      console.error('Error fetching blends:', error);
+    }
+  };
 
   const playAudio = async (fileUrl) => {
     try {
@@ -46,16 +63,17 @@ const FunStuffScreen = () => {
   };
 
   const handleBlendSelection = async (blendData) => {
-    // Stop any previously playing audio
     await stopAudio();
-
-    // Play the audio from Firebase Storage
     if (blendData.audioUrl) {
-      playAudio(blendData.audioUrl);
+      await playAudio(blendData.audioUrl);
     }
-
-    // Set the selected blend for displaying text and image
     setSelectedBlend(blendData);
+    setDefaultBlend(blendData);
+    try {
+      await AsyncStorage.setItem('defaultBlend', blendData.name);
+    } catch (error) {
+      console.error('Error storing default blend:', error);
+    }
   };
 
   return (
@@ -63,11 +81,11 @@ const FunStuffScreen = () => {
       <Text style={styles.header}>I want to feel more...</Text>
       {blendsMenu.map((item) => (
         <TouchableOpacity
-          key={item.label}
+          key={item.id}
           style={styles.button}
           onPress={() => handleBlendSelection(item)}
         >
-          <Text style={styles.buttonText}>{item.label}</Text>
+          <Text style={styles.buttonText}>{item.name}</Text>
         </TouchableOpacity>
       ))}
       {selectedBlend && (
@@ -78,45 +96,46 @@ const FunStuffScreen = () => {
       )}
     </View>
   );
-};
+}
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#f0f0f0',
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: 'blue',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginVertical: 5,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  selectedBlendContainer: {
-    marginTop: 20,
-    alignItems: 'center',
-  },
-  selectedBlendText: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-  image: {
-    width: 200,
-    height: 200,
-    resizeMode: 'cover',
-  },
+container: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#f0f0f0',
+},
+header: {
+  fontSize: 24,
+  fontWeight: 'bold',
+  marginBottom: 20,
+},
+button: {
+  backgroundColor: 'blue',
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  borderRadius: 5,
+  marginVertical: 5,
+},
+buttonText: {
+  color: 'white',
+  fontSize: 16,
+  fontWeight: 'bold',
+},
+selectedBlendContainer: {
+  marginTop: 20,
+  alignItems: 'center',
+},
+selectedBlendText: {
+  fontSize: 18,
+  marginBottom: 10,
+},
+image: {
+  width: 200,
+  height: 200,
+  resizeMode: 'cover',
+},
 });
 
 export default FunStuffScreen;
